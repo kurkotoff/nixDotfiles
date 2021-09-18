4681c9957083b414832e2c2eb994f1a9254b3e41
kurkotoff <75167883+kurkotoff@users.noreply.github.com>
2021-09-18 15:18:15 +0300

    Added home.nix

---
+++ home.nix
@@ -0,0 +1,22 @@
+
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  nixpkgs.config.allowUnfree = true;
+  imports = [
+    ./modules/programs/installs.nix
+    ./modules/programs/zsh.nix
+    ./modules/programs/git.nix
+    ./modules/programs/vscode.nix
+  ];
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home = {
+    username = "kurkotoff";
+    homeDirectory = "/home/kurkotoff";
+  };
+
+  home.stateVersion = "21.05";
+}
